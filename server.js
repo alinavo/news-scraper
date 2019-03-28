1d002b3f49d06b15778e255dbe95a548820ddc78
alinavo <alinahuynhvo@gmail.com>
2019-03-28 16:39:50 -0400

    took out logs

--- server.js
+++ server.js
@@ -5,60 +5,46 @@ var express = require("express");
 var exphbs = require("express-handlebars");
 var methodOverride = require("method-override");
 var path = require("path");
-//scraping tools
 var cheerio = require("cheerio");
 var request = require("request");
 
-//require Article and Note models
 var Article = require("./models/articles.js");
 var Note = require("./models/notes.js");
 
-//Set mongoose to leverage built in JavaScript ES6 Promises
 mongoose.Promise = Promise;
 
-//set port to 3000 or whichever one is used in environment variable
 var PORT = process.env.PORT || 3000;
 
-//set up express
 var app = express();
 
-//set the app up with body-parser and a static folder
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 
-//user method override to enable DELETE and PUT
 app.use(methodOverride("_method"));
 
-//serve static content for the app from the "public" directory in the application directory
 app.use(express.static(path.join(__dirname, "public")));
 
-//handlebars setup as view engine
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-//Database configuration with mongoose===================================================================
 var MONGODB = process.env.MONGODB_URI || "mongodb://localhost/NPRarticles";
 mongoose.connect(MONGODB);
 var db = mongoose.connection;
 
-//Show any mongoose errors
 db.on("error", function(error) {
     console.log("Mongoose Error: ", error);
 });
 
-//Once logged in to the db through mongoose, log a success message
+
 db.once("open", function() {
     console.log("Mongoose connection successful.");
-    // Listen on PORT
+
     app.listen(PORT, function() {
         console.log("App running on port 3000!");
     });
 });
 
-//Routes -- put them in a different folder and require them here
-//=======================================================================================
-//import routes and controller
 var routes = require("./controllers/controller.js");
 app.use("/", routes);
 
