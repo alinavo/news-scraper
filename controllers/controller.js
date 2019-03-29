d3e781d5d6ae36c7ba3ff57b3fa93dec8395b4c8
alinavo <alinahuynhvo@gmail.com>
2019-03-29 15:41:27 -0400

    fixed controller.js

--- controllers/controller.js
+++ controllers/controller.js
@@ -5,29 +5,31 @@ var cheerio = require("cheerio");
 var express = require("express");
 var router = express.Router();
 
-// IMPORT NOTES.JS AND ARTICLES.JS
-var Notes = require("../models/notes.js");
-var Articles = require("../models/articles.js");
 
 // db
 var db = require("../models");
 
+
+// IMPORT NOTES.JS AND ARTICLES.JS
+var Notes = require("../notes");
+var Articles = require("../articles");
+
 //GET REQUEST
-router.get("/", function(req, res) {
+router.get("/", function (req, res) {
     res.render("index");
 });
 
 //A GET REQUEST TO SCRAPE NPR
-router.get("/scrape", function(req, res) {
+router.get("/scrape", function (req, res) {
     //WEBSITE
-    request("http://www.npr.org/sections/news/", function(error, response, html) {
+    request("http://www.npr.org/sections/news/", function (error, response, html) {
         //LOAD INTO CHEERIO
         var $ = cheerio.load(html);
         var num = 0;
         //EMPTY ARRAY TO HOLD ARTICLES
         var articles = [];
         //GRAB IMAGE AND TITLE FROM ATRICLE 
-        $(".item.has-image").each(function(i, element) {
+        $(".item.has-image").each(function (i, element) {
             num = (i);
             //SAVE AND INTO RESULT EMPTY ARRAY
             var result = {};
@@ -39,29 +41,24 @@ router.get("/scrape", function(req, res) {
             //PUSH OBJECT INTO ARRAY HERE
             // if (result.title && result.link && result.teaser && result.imgLink) {
             //     articles.push(result);
-            
+
             // }
             articles.push(result);
 
             console.log("this is the information" + articles);
-        }); 
-        db.Articles.create(function(articles)
-        
-        .then(function(hbsObject){
-            
-            res.render("index", hbsObject);
-        }))
+        });
+       
         var hbsObject = { article: articles, num: num };
-        console.log( "this is a random variable...maybe" + hbsObject)
+        console.log("this is a random variable...maybe" + hbsObject)
         //REDIRECTS INTO ARTICLE PAGE
     });
 
 });
 
 //POST ROUTE TO SAVE ARTICLES
-router.post("/save", function(req, res) {
+router.post("/save", function (req, res) {
     var newArticle = new Articles(req.body);
-    newArticle.save(function(error, doc) {
+    newArticle.save(function (error, doc) {
         if (error) {
             console.log(error);
         } else {
@@ -72,31 +69,31 @@ router.post("/save", function(req, res) {
 
 //ARTICLES TO DISPLAY ARTICLES
 //DISPLAYS SCRAPED ARTICLES
-router.get("/articles", function(req, res) {
+router.get("/articles", function (req, res) {
     //GRABS DOCS IN ARTICLES ARRAY
     //SORTS THEM AND POPULATES WITH NOTE
     Articles.find().sort({ "_id": -1 })
         .populate("note").
-    exec(function(err, doc) {
-        console.log(doc);
-        //IF ERROR LOG 
-        if (err) {
-            console.log(err);
-        }
-        //RETURN TO BROWSER AS JSON OBJECT
-        else {
-            var hbsObject = {
-                savedArticles: doc
-            };
-            res.render("saved", hbsObject);
-        }
-    });
+        exec(function (err, doc) {
+            console.log(doc);
+            //IF ERROR LOG 
+            if (err) {
+                console.log(err);
+            }
+            //RETURN TO BROWSER AS JSON OBJECT
+            else {
+                var hbsObject = {
+                    savedArticles: doc
+                };
+                res.render("saved", hbsObject);
+            }
+        });
 });
 
 //DELETES ARTICLE BY ID
-router.delete("/delete/:id", function(req, res) {
+router.delete("/delete/:id", function (req, res) {
     //DELETES NOTES OF ARTICLE
-    Articles.findOne({ "_id": req.params.id }, function(err, data) {
+    Articles.findOne({ "_id": req.params.id }, function (err, data) {
         if (err) {
             console.log(err);
         } else if (data.note) {
@@ -104,7 +101,7 @@ router.delete("/delete/:id", function(req, res) {
             var noteIDs = data.note;
             //LOOPS THROUGH NOTE ARRAY
             for (var i = 0; i < noteIDs.length; i++) {
-                Notes.findByIdAndRemove(noteIDs[i], function(error, doc) {
+                Notes.findByIdAndRemove(noteIDs[i], function (error, doc) {
                     if (error) {
                         console.log(error)
                     }
@@ -114,7 +111,7 @@ router.delete("/delete/:id", function(req, res) {
     });
 
     //DELETES ARTICLE USING FIND BY ID AND REMOVE
-    Articles.findByIdAndRemove(req.params.id, function(error, doc) {
+    Articles.findByIdAndRemove(req.params.id, function (error, doc) {
         if (error) {
             console.log(error);
         }
@@ -123,18 +120,18 @@ router.delete("/delete/:id", function(req, res) {
 });
 
 //POST ROUTE TO ADD NEW NOTE
-router.post("/articles/:id", function(req, res) {
+router.post("/articles/:id", function (req, res) {
     //CREATES A NOTE AND REQ.BODY TO ENTRY
     var newNote = new Notes(req.body);
     //SAVES NOTES TO NEW DB
-    newNote.save(function(error, doc) {
+    newNote.save(function (error, doc) {
         if (error) {
             console.log(error);
         } else {
             //UPDATES ARTICLE BY NOTES
             Articles.findOneAndUpdate({ "_id": req.params.id }, { $push: { "note": doc._id } }, { new: true })
                 //ERROR 
-                .exec(function(err, doc) {
+                .exec(function (err, doc) {
                     if (err) {
                         console.log(err);
                     } else {
@@ -146,10 +143,10 @@ router.post("/articles/:id", function(req, res) {
 });
 
 //DELETES A NOTE
-router.delete("/delete/notes/:id", function(req, res) {
+router.delete("/delete/notes/:id", function (req, res) {
     var id = req.params.id;
 
-    Notes.findByIdAndRemove({ "_id": req.params.id }, function(err, doc) {
+    Notes.findByIdAndRemove({ "_id": req.params.id }, function (err, doc) {
         if (err) {
             console.log(err);
         }
